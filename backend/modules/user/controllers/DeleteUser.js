e1575ca481158e49a9e9c6d793b697c3ed5fdaf1
aliraza <chaudaryaliraza0@gmail.com>
2025-02-08 15:19:26 +0500

    fix DeleteUser

--- backend/modules/user/controllers/DeleteUser.js
+++ backend/modules/user/controllers/DeleteUser.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const DeleteUser = async(req, res) => {
     const userModel = mongoose.model('user');
     const bikeModel = mongoose.model('bike');
+    const bookingModel = mongoose.model('booking');
     const userId = req.params.id;
 
     if (!userId) {
@@ -14,6 +15,7 @@ const DeleteUser = async(req, res) => {
     }
 
     const bike = await bikeModel.deleteMany({ user: userId });
+    const booking = await bookingModel.deleteMany({ user: userId });
     const user = await userModel.deleteOne({ _id: userId });
 
     res.status(200).json({ status: "Success", message: "User and Associated Bikes Deleted Successfully"});
